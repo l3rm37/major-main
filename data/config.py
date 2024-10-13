9b3a306dd416ac39fef1bfe42ef7c8d894c29417
l3rm37 <lermovl@gmail.com>
2024-10-13 18:20:02 +0700

    Add files via upload

---
+++ data/config.py
@@ -0,0 +1,49 @@
+# api id, hash
+API_ID = 11111111
+API_HASH = 'Your api hash'
+
+USE_TG_BOT = False # True if you want use tg, else False
+BOT_TOKEN = '' # API TOKEN get in @BotFather
+CHAT_ID = '' # Your telegram id
+
+# задержка между подключениями к аккаунтам
+ACC_DELAY = [5, 150]
+
+# тип прокси
+PROXY_TYPE = "socks5" # http/socks5
+
+# папка с сессиями (не менять)
+WORKDIR = "sessions/"
+
+# использование прокси
+USE_PROXY = True # True/False
+# проверять прокси
+CHECK_PROXY = True
+
+# реф код, идет после startapp=
+REF_CODE = '707649803'
+
+# задержка между тасками
+TASK_SLEEP = [30,60] #[min,max]
+
+# задержка между минииграми
+GAME_SLEEP = [30,120] #[min,max]
+
+# айди тасков которые проверяются
+BLACKLIST_TASKS = {33,21,27,20,36,80,77}
+
+# играть в данную мини игру True/False
+HOLD_COIN = True
+ROULETTE = True
+SWIPE_COIN = True
+
+# мини задержки
+MINI_SLEEP = [5,15] #[min,max]
+
+#задержка между циклами
+BIG_SLEEP = [5*60*60,8*60*60]
+
+# вступать в сквад
+JOIN_SQUAD = True
+
+hello ='''l3rm37'''
